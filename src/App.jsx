c72ee9b69c2e4d45c96ea4f26508e3a1300f5141
Nitin Vinayak <nitinvinayak@Nitins-MacBook-Pro.local>
2024-07-01 13:50:13 +0530

    1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
 
 import './App.css'
-import Navbar from './assets/components/Navbar'   
+import Navbar from './assets/components/Navbar/Navbar'   
 
 function App() {
   const [count, setCount] = useState(0)
-  
+
   const navbarHome = ['Login', 'About']
   
   return (
     <>
       <Navbar items = {navbarHome} />
+      <div className="heroDiv">
+        <h3>LEDGER</h3> 
+        <p>BY STELLAR</p>
+      </div>
     </>
   )
 }
