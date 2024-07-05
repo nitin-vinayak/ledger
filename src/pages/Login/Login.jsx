d5d468dc42b200857df9b3829930b90c9de48115
Nitin <nitinvinayak.m@gmail.com>
2024-07-06 01:51:52 +0530

    1.3

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,3 +1,20 @@
-export default function LoginFunction(){
-    return <p>Login</p>
+import "./Login.css";
+
+export default function LoginFunction() {
+  function submitHandler(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <>
+      <form onSubmit={submitHandler}>
+        <div className="parent">
+          <h3>Login</h3>
+          <input type="email" name="email" id="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <button value="Submit"> Login </button>
+        </div>
+      </form>
+    </>
+  );
 }
